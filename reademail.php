17bb2d6d87c9c9285d200977f1d3e1bf9725a86b
shkoolnik <shkoolnik@gmail.com>
2019-06-05 21:25:15 +0300

    дз5

---
+++ reademail.php
@@ -0,0 +1,15 @@
+<?php
+<table>
+    <tr>
+
+$fp = fopen("user.txt", 'r');
+if ($fp)
+{
+    while (!feof($fp))
+    {
+        $mytext = fgets($fp, 999);
+        echo $mytext."<br />";
+    }
+}
+else echo "Ошибка при открытии файла";
+fclose($fp);
